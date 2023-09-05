2932a2f7aa3baf72aa5eb4f7833ce198f18ba4c8
Fransiskus Aldo <rochesterfransiskusaldo@gmail.com>
2023-09-05 16:08:28 -0400

    add showdata base on date selected

---
+++ App/chart.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Chart</title>
+</head>
+<body>
+  <section>
+    <form action="./showData.php" method="POST">
+      <input type="date" name="date">
+      <input type="submit" value="Submit">
+    </form>
+  </section>
+</body>
+</html>
--- App/showData.php
+++ App/showData.php
@@ -5,25 +5,32 @@ require_once "./_includes/db_connect.php";
 
 //(`ticket_id`, `adult`, `kids_under_4`, `kids_4_to_18`, `senior_over60`, `date`)
 
-$statement = mysqli_prepare($link, "SELECT adult, kids_under_4, kids_4_to_18, senior_over60, date FROM audiences ORDER BY date DESC");
+
+if(isset($_REQUEST["date"])){
+  $selectedDate = $_REQUEST["date"];
+
+  $statement = mysqli_prepare($link, "SELECT ticket_id, adult, kids_under_4, kids_4_to_18, senior_over60, date FROM audiences WHERE date='$selectedDate'");
+  // echo $selectedDate;
+
+// $statement = mysqli_prepare($link, "SELECT * FROM audiences WHERE date = $selectedDate");
 
 //execute the query statement
-mysqli_stmt_execute($statement);
+  mysqli_stmt_execute($statement);
 
 //get the result
-$result = mysqli_stmt_get_result($statement);
+  $result = mysqli_stmt_get_result($statement);
 
 //loop through the row and give back the result as a json format
 
-while($row = mysqli_fetch_assoc($result)){
-  $results[] = $row;
+  while($row = mysqli_fetch_assoc($result)){
+    $results[] = $row;
+  }
+
+  echo json_encode($results);
+
+  mysqli_close($link);
+
 }
-
-echo json_encode($results);
-
-mysqli_close($link);
-
-
 
 
 
