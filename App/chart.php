a73d24174d9ac1cab18660c26aaedf3d26fc72a5
Fransiskus Aldo <rochesterfransiskusaldo@gmail.com>
2023-09-06 13:18:17 -0400

    add fetching data to chart.php and store the results into variable

--- App/chart.php
+++ App/chart.php
@@ -7,10 +7,62 @@
 </head>
 <body>
   <section>
-    <form action="./showData.php" method="POST">
+    <form action="chart.php" method="POST">
       <input type="date" name="date">
       <input type="submit" value="Submit">
     </form>
   </section>
 </body>
 </html>
+
+<?php 
+
+//require db connection from db_connect
+require_once "./_includes/db_connect.php";
+
+//(`ticket_id`, `adult`, `kids_under_4`, `kids_4_to_18`, `senior_over60`, `date`)
+
+
+if(isset($_REQUEST["date"])){
+  $selectedDate = $_REQUEST["date"];
+
+  //statement to display only total of each audience to that day.
+  $statement = mysqli_prepare($link, "SELECT ticket_id, SUM(adult), SUM(kids_under_4), SUM(kids_4_to_18), SUM(senior_over60) FROM audiences WHERE date='$selectedDate'");
+
+  //statement to display all data
+  // $statement = mysqli_prepare($link, "SELECT ticket_id, adult, kids_under_4, kids_4_to_18, senior_over60, date FROM audiences WHERE date='$selectedDate'");
+  // echo $selectedDate;
+
+// $statement = mysqli_prepare($link, "SELECT * FROM audiences WHERE date = $selectedDate");
+
+//execute the query statement
+  mysqli_stmt_execute($statement);
+
+//get the result
+  $result = mysqli_stmt_get_result($statement);
+
+//loop through the row and give back the result as a json format
+  
+  
+
+  while($row = mysqli_fetch_assoc($result)){
+
+    $results[] = $row;
+  }
+
+  //add the result into variable;
+  $adult = $results[0]["adult"];
+  $kidUnder4 = $results[0]["kids_under_4"];
+  $kidsOver4 = $results[0]["kids_4_to_18"];
+  $senior = $results[0]["senior_over60"];
+
+  // echo json_encode($results);
+  // echo $results[0]["ticket_id"];
+
+  mysqli_close($link);
+
+  
+
+}
+
+?>
--- App/showData.php
+++ App/showData.php
@@ -9,10 +9,14 @@ require_once "./_includes/db_connect.php";
 if(isset($_REQUEST["date"])){
   $selectedDate = $_REQUEST["date"];
 
-  $statement = mysqli_prepare($link, "SELECT ticket_id, adult, kids_under_4, kids_4_to_18, senior_over60, date FROM audiences WHERE date='$selectedDate'");
+  //statement to display only total of each audience to that day.
+  // $statement = mysqli_prepare($link, "SELECT ticket_id, SUM(adult), SUM(kids_under_4), SUM(kids_4_to_18), SUM(senior_over60) FROM audiences WHERE date='$selectedDate'");
+
+  //statement to display all data
+  // $statement = mysqli_prepare($link, "SELECT ticket_id, adult, kids_under_4, kids_4_to_18, senior_over60, date FROM audiences WHERE date='$selectedDate'");
   // echo $selectedDate;
 
-// $statement = mysqli_prepare($link, "SELECT * FROM audiences WHERE date = $selectedDate");
+$statement = mysqli_prepare($link, "SELECT * FROM audiences ORDER BY date DESC");
 
 //execute the query statement
   mysqli_stmt_execute($statement);
@@ -21,14 +25,17 @@ if(isset($_REQUEST["date"])){
   $result = mysqli_stmt_get_result($statement);
 
 //loop through the row and give back the result as a json format
-
+  
   while($row = mysqli_fetch_assoc($result)){
+
     $results[] = $row;
   }
 
   echo json_encode($results);
 
   mysqli_close($link);
+
+  
 
 }
 
