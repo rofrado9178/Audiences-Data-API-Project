58326557bb0e9db6ad262a2d56dd5e423f587709
Fransiskus Aldo <rochesterfransiskusaldo@gmail.com>
2023-09-07 13:43:19 -0400

    refactor the charts functionality

--- App/js/main.js
+++ App/js/main.js
@@ -1,5 +1,99 @@
-const myForm = document.getElementById("form-submit");
+async function dataFetch(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log(data);
+}
+
+async function insertData(data, url) {
+  const response = await fetch(url, {
+    method: "POST",
+    body: data,
+  });
+
+  const responseData = await response.json();
+  console.log(responseData);
+
+  showChart(responseData);
+}
+
+const myForm = document.querySelector("#form-submit");
 
 myForm.addEventListener("submit", function (e) {
   e.preventDefault();
+
+  const url = "./totalAudience.php";
+  const formData = new FormData(myForm);
+
+  insertData(formData, url);
 });
+
+// // CHART
+
+async function showChart(datas) {
+  console.log(datas);
+  const adult = datas.map((data) => data["SUM(adult)"]);
+  const kidsUnder4 = datas.map((data) => data["SUM(kids_under_4)"]);
+  const kidsOver4 = datas.map((data) => data["SUM(kids_4_to_18)"]);
+  const senior = datas.map((data) => data["SUM(senior_over60)"]);
+  // Sample data for the charts
+  const barChartData = {
+    labels: ["Adult", "Kids Under 4 Years", "Kids 4 to 18 Years", "Senior"],
+    datasets: [
+      {
+        label: "Bar Chart",
+        backgroundColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(11,156,49,1)",
+        ],
+        borderColor: "rgba(0, 0, 0, 1)",
+        borderWidth: 1,
+        data: [adult, kidsUnder4, kidsOver4, senior],
+      },
+    ],
+  };
+  // Create the bar chart
+  const barChartCanvas = document.getElementById("barChart").getContext("2d");
+  const barChart = new Chart(barChartCanvas, {
+    type: "bar",
+    data: barChartData,
+    options: {
+      indexAxis: "y",
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+  //pie chart
+  const pieChartData = {
+    labels: ["Adults", "Kids Under 4", "Kids 4 to 18 Years", "Senior"],
+    datasets: [
+      {
+        data: [adult, kidsUnder4, kidsOver4, senior],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(11,156,49,0.4)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(11,156,49,1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  // Create the pie chart
+  const pieChartCanvas = document.getElementById("pieChart").getContext("2d");
+  const pieChart = new Chart(pieChartCanvas, {
+    type: "pie",
+    data: pieChartData,
+  });
+}
---
+++ App/totalAudience.php
@@ -0,0 +1,48 @@
+
+<?php 
+
+//require db connection from db_connect
+require_once "./_includes/db_connect.php";
+
+//(`ticket_id`, `adult`, `kids_under_4`, `kids_4_to_18`, `senior_over60`, `date`)
+
+
+if(isset($_REQUEST["date"])){
+  $selectedDate = trim(strip_tags($_REQUEST["date"]));
+  // echo $selectedDate;
+
+  //statement to display only total of each audience to that day.
+  $statement = mysqli_prepare($link, "SELECT ticket_id, SUM(adult), SUM(kids_under_4), SUM(kids_4_to_18), SUM(senior_over60) FROM audiences WHERE date='$selectedDate'");
+
+  //statement to display all data
+  // $statement = mysqli_prepare($link, "SELECT ticket_id, adult, kids_under_4, kids_4_to_18, senior_over60, date FROM audiences WHERE date='$selectedDate'");
+  // echo $selectedDate;
+
+// $statement = mysqli_prepare($link, "SELECT * FROM audiences WHERE date = $selectedDate");
+
+//execute the query statement
+  mysqli_stmt_execute($statement);
+
+//get the result
+  $result = mysqli_stmt_get_result($statement);
+
+//loop through the row and give back the result as a json format
+  
+  
+
+  while($row = mysqli_fetch_assoc($result)){
+
+    $results[] = $row;
+  }
+
+  //add the result into variable;
+ 
+
+  echo json_encode($results);
+
+
+  mysqli_close($link);
+
+  
+
+}
