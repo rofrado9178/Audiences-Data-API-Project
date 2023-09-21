9319c1297a6685382a27c4aec5ece8cadda762c0
Fransiskus Aldo <rochesterfransiskusaldo@gmail.com>
2023-09-21 13:03:37 -0400

    add user registration

---
+++ App/js/userRegister.js
@@ -0,0 +1,29 @@
+async function registerUser(data, url) {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: data,
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const responseData = await response.json();
+
+    // console.log(JSON.parse(responseData));
+  } catch (error) {
+    console.error("Error oocurred", error);
+  }
+}
+
+const register = document.querySelector("#register");
+
+register.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const registerUrl = "./register.php";
+
+  const userRegistration = new FormData(register);
+  console.log(userRegistration);
+  registerUser(userRegistration, registerUrl);
+});
---
+++ App/register.php
@@ -0,0 +1,66 @@
+<?php 
+
+require_once "./_includes/db_connect.php";
+
+
+if(isset($_REQUEST["username"]) && isset($_REQUEST["password_1"]) && isset($_REQUEST["password_2"])){
+  $query = "INSERT INTO users(username, password) VALUES (?, ?)";
+
+  $insertData = 0;
+  $results = [];
+
+  $password1 = $_REQUEST["password_1"];
+  $password2 = $_REQUEST["password_2"];
+  if($password1 != $password2){
+    $results = [
+      "error" => "Password does not match"
+    ];
+    echo($results);
+  }
+  $encryptedPassword = password_hash($password1, PASSWORD_DEFAULT);
+
+  if($statement = mysqli_prepare($link, $query)){
+    mysqli_stmt_bind_param($statement,"ss", $_REQUEST["username"], $password1);
+  
+    mysqli_stmt_execute($statement);
+  
+    $insertData = mysqli_stmt_affected_rows($statement);
+  
+    if($insertData > 0){
+      $results = [
+        "Success" => "Registration Succesful"
+      ];
+      echo  json_encode($results);
+    }
+    else{
+      throw new Exception("Failed to register");
+    }
+  
+  }
+}
+  // $insertData = 0;
+  // $results = [];
+  // $query = "INSERT INTO users(username,  password) VALUES (?, ?)";
+  // if($statement = mysqli_prepare($link, $query)){
+  //   mysqli_stmt_bind_param($statement,"ss", $_REQUEST["username"],$_REQUEST["password_1"]);
+  
+  //   mysqli_stmt_execute($statement);
+  
+  //   $insertData = mysqli_stmt_affected_rows($statement);
+  
+  //   if($insertData > 0){
+  //     echo  json_encode("Register Success");
+  //   }
+  //   else{
+  //     throw new Exception("Failed to register");
+  //   }
+  
+  // }
+
+
+
+
+
+
+
+?>
